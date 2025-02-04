80b4f87ec2b466456fc8a29feb87d7ea426bc05c
Perlam99 <plmolina1@buffs.wtamu.edu>
2025-02-03 19:34:40 -0600

    Homework 2 code file

---
+++ Homework2.cs
@@ -0,0 +1,71 @@
+﻿namespace Homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Code for Q1
+      /* Console.WriteLine("Please input a letter grade: ");
+        string letter = Console.ReadLine();
+        if(letter == "A") {
+            Console.WriteLine("GPA point: 4");
+        }
+        else if (letter == "B") {
+            Console.WriteLine("GPA point: 3");
+        }
+        else if (letter == "C") {
+            Console.WriteLine("GPA point: 2");
+        }
+        else if (letter == "D") {
+            Console.WriteLine("GPA point: 1");
+        }
+        else if (letter == "F") {
+            Console.WriteLine("GPA point: 0");
+        }
+        else{
+            Console.WriteLine("Wrong Letter Grade!");
+        }*/
+           
+           //Code for Q2
+           
+          /* Console.WriteLine("Please input the first num:");
+           string input1 = Console.ReadLine();
+           int num1 = Convert.ToInt16(input1);
+
+           Console.WriteLine("Please input the second num:");
+           string input2 = Console.ReadLine();
+           int num2 = Convert.ToInt16(input2);
+
+           Console.WriteLine("Please input the third num:");
+           string input3 = Console.ReadLine();
+           int num3 = Convert.ToInt16(input3);
+
+
+            int smallest;
+
+            if (num1 < num2)
+            {
+               if (num1 < num3)
+               {
+                smallest = num1;
+               } 
+               else
+               {
+                smallest = num3;
+               }
+            }
+            else
+            {
+                if (num2 < num3)
+                {
+                    smallest = num2;
+                }
+                else        
+                {
+                    smallest = num3;
+                }
+            }
+            Console.WriteLine("The smallest value is:" + smallest);
+            */
+    }
+}
