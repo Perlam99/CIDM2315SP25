2e3d137313d2f032e583f2c6f7982fbf315b1d9c
Perlam99 <plmolina1@buffs.wtamu.edu>
2025-04-14 13:43:08 -0500

    Update Homework9.cs

--- Homework9.cs
+++ Homework9.cs
@@ -3,6 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+class Student
+{
+    public static List<Student> student_list = new List<Student>();
+    private int studentID { get; set; }
+    private string studentName { get; set; }
+    public void PrintInfo(){
+        Console.WriteLine($"Student ID: {studentID}, Student Name: {studentName}");
+    }
+    
+    public string GetName()
+    {
+        return studentName;
+    }
+
+    public int GetID()
+    {
+        return studentID;
+    }
+        public Student(int inputID, string inputName)
+        {
+        studentID = inputID;
+        studentName = inputName;
+        student_list.Add(this);
+    }
+
+}
+
+
 
 class Program
 {
